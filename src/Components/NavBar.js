c66a6e4e578d16b882c7964a4357935b87d4a51e
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-18 13:35:25 -0400

    Add color palette

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,4 +1,10 @@
+import { Link } from "react-router-dom";
 
 export default function NavBar() {
-    
+    return (
+        <div>
+            <h1>Budget App</h1>
+            <Link to={"/transactions/new"}>NEW TRANSACTION</Link>
+        </div>
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#CDFAEB",
+      secondary: "#CEEBEC",
+      gray: "#96ADA9",
+      dark: "#96ADA9"
+    },
     extend: {},
   },
   plugins: [],
