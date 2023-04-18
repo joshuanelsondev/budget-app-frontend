beb69e3b10401e9b790272c5183d27b3e041fb4a
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-18 14:09:48 -0400

    Style the navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 
-export default function NavBar() {
+export default function NavBar() { 
     return (
-        <div>
-            <h1>Budget App</h1>
-            <Link to={"/transactions/new"}>NEW TRANSACTION</Link>
+        <div className="bg-background h-28 w-full flex justify-between items-center px-10 text-dark">
+            <Link to={"/transactions"} className="text-4xl font-semibold">Budget App</Link>
+            <Link to={"/transactions/new"} className="border border-gray rounded text-xs font-bold py-2 px-6">NEW TRANSACTION</Link>
         </div>
     )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      'primary': "#CDFAEB",
-      'secondary': "#CEEBEC",
-      'gray': "#96ADA9",
-      'dark': "#96ADA9"
+      primary: "#CDFAEB",
+      secondary: "#CEEBEC",
+      gray: "#96ADA9",
+      dark: "#303738",
+      background: "hsla(160, 82%, 89%, 1)",
     },
     extend: {},
   },
