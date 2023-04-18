b940984c7e8d8f1e9a3ff6be82a9c3c60753d564
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-18 13:45:22 -0400

    Update files

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      primary: "#CDFAEB",
-      secondary: "#CEEBEC",
-      gray: "#96ADA9",
-      dark: "#96ADA9"
+      'primary': "#CDFAEB",
+      'secondary': "#CEEBEC",
+      'gray': "#96ADA9",
+      'dark': "#96ADA9"
     },
     extend: {},
   },
