996d85566d9cdb4307750088383712c5901bfec2
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-20 18:59:56 -0400

    Edit Index.js and Transactions.js

--- src/Components/Transactions.js
+++ src/Components/Transactions.js
@@ -2,16 +2,22 @@ import { useEffect } from "react";
 import axios from "axios";
 import Transaction from "./Transaction";
 
-export default function Transactions({ transactions, setTransactions }) {
-    
+export default function Transactions({ transactions, setTransactions, setTotal }) {
+    const calculateTotal = (transactions) => {
+    console.log(transactions);
+    return transactions.reduce((total, transaction) => {
+        return total + Number(transaction.amount);
+    }, 0);
+    }; 
 
     useEffect(() => {
         axios.get(`${process.env.REACT_APP_API_URL}/transactions`).then((res) => {
             setTransactions(res.data);
+            setTotal(calculateTotal(res.data))
         }).catch((error) => {
             console.log(error);
         });
-    }, [setTransactions]);   
+    }, [setTransactions, setTotal]);   
     
     return (
         <div className="flex flex-col items-between mx-16">
--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -3,18 +3,14 @@ import Transactions from "../Components/Transactions";
 
 export default function Index() {
   const [transactions, setTransactions] = useState([]);
+  const [total, setTotal] = useState(0)
 
-
-  const calculateTotal = (transactions) => {
-    return transactions.reduce((total, transaction) => {
-      return total + Number(transaction.amount)
-    }, 0)
-  } 
+ 
   return (
 
     <div className="flex flex-col mt-8 gap-8 w-8/12 min-w-[700px]">
-      <h2 className="text-5xl text-center text-dark pt-4 pl-12 w-full">Bank Account Total: ${transactions ? calculateTotal(transactions) : 0}</h2>
-      <Transactions transactions={transactions} setTransactions={setTransactions} />
+      <h2 className="text-5xl text-center text-dark pt-4 pl-12 w-full">Bank Account Total: ${total}</h2>
+      <Transactions transactions={transactions} setTransactions={setTransactions} setTotal={setTotal}/>
     </div>
 
   );
