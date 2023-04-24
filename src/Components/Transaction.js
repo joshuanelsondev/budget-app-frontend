4351963823cc69f8e55a9167b6fd6622b2062512
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-23 20:37:42 -0400

    Edit Index.js

--- src/Components/Transaction.js
+++ src/Components/Transaction.js
@@ -10,18 +10,19 @@ export default function Transaction({ transaction }) {
   };
 
   return (
+    
     <div className="grid grid-cols-[100px_1fr_auto] gap-2 pb-4">
       <p className="text-dark justify-self-start border-b border-gray pb-4 w-full">
         {formatDate(transaction.date)}
       </p>
-      <Link
-        to={`/transactions/${transaction.id}`}
-        className="text-link font-semibold justify-self-start border-b border-gray pb-4 w-full"
-      >
-        <span className="hover:underline underline-offset-2">
+      <span className="text-link font-semibold justify-self-start border-b border-gray pb-4 w-full">
+        <Link
+          to={`/transactions/${transaction.id}`}
+          className="hover:underline underline-offset-2"
+        >
           {transaction.item_name}
-        </span>
-      </Link>
+        </Link>
+      </span>
       <div className="border-b border-gray w-16 flex justify-end">
         <p className="text-dark pb-4">{`$${transaction.amount}`}</p>
       </div>
--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import Transactions from "../Components/Transactions";
 
 export default function Index() {
@@ -7,11 +8,23 @@ export default function Index() {
 
  
   return (
-
     <div className="flex flex-col mt-8 gap-8 w-8/12 min-w-[700px]">
-      <h2 className="text-5xl text-center text-dark pt-4 pl-12 w-full">Bank Account Total: ${total}</h2>
-      <Transactions transactions={transactions} setTransactions={setTransactions} setTotal={setTotal}/>
+      <h2 className="text-5xl text-center text-dark pt-4 pl-12 w-full">
+        Bank Account Total: ${total}
+      </h2>
+      <Transactions
+        transactions={transactions}
+        setTransactions={setTransactions}
+        setTotal={setTotal}
+      />
+      <div className="flex justify-start gap-8 ml-16">
+        <Link
+          className="border border-gray  rounded-lg text-sm py-2 px-4 hover:bg-primary "
+          to={"/"}
+        >
+          BACK
+        </Link>
+      </div>
     </div>
-
   );
 }
