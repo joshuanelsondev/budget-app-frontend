1d290d4bf0489e743e0cfbb7fc44897e4c275c6e
joshuanelsondev <joshuanelsondev@gmail.com>
2023-04-28 21:48:31 -0400

    Add modal to TransactionDetails.js

--- src/Components/TransactionDetails.js
+++ src/Components/TransactionDetails.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 export default function TransactionDetails() {
     const [transaction, setTransaction] = useState([]);
+    const [showModal, setShowModal] = useState(false);
     const navigate = useNavigate();
     let { id } = useParams();
 
@@ -30,12 +31,21 @@ export default function TransactionDetails() {
         <div className="flex flex-col gap-4 p-4">
           <h1 className="text-4xl">{transaction.item_name}</h1>
           <h2 className="border-b pb-4">
-            <span className="font-bold">Amount: </span> 
+            <span className="font-bold">Amount: </span>
             {`$${transaction.amount}`}
           </h2>
-          <h2 className="border-b pb-4"><span className="font-bold">Date: </span>{transaction.date}</h2>
-          <h2 className="border-b pb-4"><span className="font-bold">From: </span>{transaction.from}</h2>
-          <h2 className="border-b pb-4"><span className="font-bold">Category: </span>{transaction.category}</h2>
+          <h2 className="border-b pb-4">
+            <span className="font-bold">Date: </span>
+            {transaction.date}
+          </h2>
+          <h2 className="border-b pb-4">
+            <span className="font-bold">From: </span>
+            {transaction.from}
+          </h2>
+          <h2 className="border-b pb-4">
+            <span className="font-bold">Category: </span>
+            {transaction.category}
+          </h2>
         </div>
         <div className="flex justify-center gap-8 mt-8">
           <Link
@@ -52,11 +62,22 @@ export default function TransactionDetails() {
           </Link>
           <button
             className="border rounded-lg text-sm py-2 px-4 hover:bg-primary "
-            onClick={handleDelete}
+            onClick={() => setShowModal(true)}
           >
             DELETE
           </button>
         </div>
+        {showModal && (
+          <div className="rounded-xl bg-primary shadow-[2px_3px_8px_gray] self-center p-4 w-fit mt-8">
+            <div>
+              <h2 className="text-lg font-bold text-dark">Are you sure you want to delete this?</h2>
+              <div className="flex justify-center gap-4 mt-4">
+                <button className="rounded w-10 text-sm font-bold hover:bg-secondary" onClick={handleDelete}>YES</button>
+                <button className="rounded w-10 text-sm font-bold hover:bg-secondary" onClick={() => setShowModal(false)}>NO</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     );
 }
